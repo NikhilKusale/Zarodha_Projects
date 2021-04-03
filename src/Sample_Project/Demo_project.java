20817e3a9919f62ad34f1c85772f96052716e40b
Venkys <Venkys@DESKTOP-NIIH1LL>
2021-04-03 21:18:19 +0530

    [Nikhil]-Adding Project  to GitHub Server

---
+++ src/Sample_Project/Demo_project.java
@@ -0,0 +1,13 @@
+package Sample_Project;
+
+public class Demo_project {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		System.out.println("Hi....EveryOne ");
+		
+		
+	}
+
+}
